fdbfbde920f05b5e74bfe186d6da8f49bed72f43
altafhussain110 <48952616+altafhussain110@users.noreply.github.com>
2020-03-25 00:20:25 +0500

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,58 +1,4 @@
-// const TypeWriter = function(txtElement, words, wait = 3000) {
-//   this.txtElement = txtElement;
-//   this.words = words;
-//   this.txt = '';
-//   this.wordIndex = 0;
-//   this.wait = parseInt(wait, 10);
-//   this.type();
-//   this.isDeleting = false;
-// }
 
-// // Type Method
-// TypeWriter.prototype.type = function() {
-//   // Current index of word
-//   const current = this.wordIndex % this.words.length;
-//   // Get full text of current word
-//   const fullTxt = this.words[current];
-
-//   // Check if deleting
-//   if(this.isDeleting) {
-//     // Remove char
-//     this.txt = fullTxt.substring(0, this.txt.length - 1);
-//   } else {
-//     // Add char
-//     this.txt = fullTxt.substring(0, this.txt.length + 1);
-//   }
-
-//   // Insert txt into element
-//   this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
-
-//   // Initial Type Speed
-//   let typeSpeed = 300;
-
-//   if(this.isDeleting) {
-//     typeSpeed /= 2;
-//   }
-
-//   // If word is complete
-//   if(!this.isDeleting && this.txt === fullTxt) {
-//     // Make pause at end
-//     typeSpeed = this.wait;
-//     // Set delete to true
-//     this.isDeleting = true;
-//   } else if(this.isDeleting && this.txt === '') {
-//     this.isDeleting = false;
-//     // Move to next word
-//     this.wordIndex++;
-//     // Pause before start typing
-//     typeSpeed = 500;
-//   }
-
-//   setTimeout(() => this.type(), typeSpeed);
-// }
-
-
-// ES6 Class
 class TypeWriter {
     constructor(txtElement, words, wait = 3000) {
       this.txtElement = txtElement;
@@ -149,23 +95,4 @@ class TypeWriter {
     
     
     });
-  
-//     Function myFun(){
-// var correct_var =/ ^[A-Za-z] +$/;
-// var a =document.getElementById("message"). Value;
-// if (a===""){
-//   document.getElementById ("error").innerHTML="** please fill the answer";
-//   retrun false;
-// }
-
-// if(a.length<3){
-// document.getElementById("error").innerHTML='**username must be 3charchter';
-// return false;
-// }
-// if(a.length>20){
-//   document.getElementById ("error").innerHTML="**usernsme mustbe  3charchter";
-//   return false;
-// }
-
-
-//     }
+ 
